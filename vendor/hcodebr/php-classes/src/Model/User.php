e919965fd4aa501455a4d59ac9d3f2e07e8e6cc2
Hugo Neves <hugoneves@MacBook-Air-de-Hugo.local>
2020-12-31 20:24:52 -0300

    Carrinho de Compras

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -5,6 +5,7 @@ namespace Hcode\Model;
 use \Hcode\DB\Sql;
 use \Hcode\Model;
 use \Hcode\Mailer;
+use \Hcode\Model\User;
 
 class User extends Model {
 
@@ -247,7 +248,7 @@ class User extends Model {
 
 				} else {
 
-					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+					$link = "http://www.hcodecommerce.com.br:8888/forgot/reset?code=$code";
 					
 				}				
 
